cf015b0fd5a929ae4e7a84f2bdb12d47fd52a7ac
Partha Sarthi <partha.sarthi@gmail.com>
2017-11-03 13:18:54 +0530

    Upgraded Project with latest android studi

--- RideShareApp/app/src/main/java/com/digitusrevolution/rideshare/LandingPageActivity.java
+++ RideShareApp/app/src/main/java/com/digitusrevolution/rideshare/LandingPageActivity.java
@@ -3,11 +3,11 @@ package com.digitusrevolution.rideshare;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class LandlingPageActivity extends AppCompatActivity {
+public class LandingPageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_landling_page);
+        setContentView(R.layout.activity_landing_page);
     }
 }
