db736ae98d26594bc6f8a8d427614cf798ea7c03
Partha Sarthi <partha.sarthi@gmail.com>
2017-11-16 18:42:18 +0530

    Basic Navigation Drawer Template is setup

---
+++ RideShareApp/app/src/main/java/com/digitusrevolution/rideshare/activity/HomePageActivity.java
@@ -0,0 +1,77 @@
+package com.digitusrevolution.rideshare.activity;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.digitusrevolution.rideshare.R;
+
+public class HomePageActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //This will set up action bar for this activity
+        setSupportActionBar(toolbar);
+
+        //This will show up Hamburger icon on action bar and on click of it, drawer would open up
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        //This will set up listener on Navigation items
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        Toast.makeText(this,"onBackPressed",Toast.LENGTH_SHORT).show();
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        Toast.makeText(this,item.getTitle(),Toast.LENGTH_SHORT).show();
+        if (id == R.id.nav_home) {
+
+        } else if (id == R.id.nav_rides) {
+
+        } else if (id == R.id.nav_payments) {
+
+        } else if (id == R.id.nav_friends) {
+
+        } else if (id == R.id.nav_groups) {
+
+        } else if (id == R.id.nav_vehicles) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
