06a9d31612316da4347c12db28b8f95df57a08c7
Partha Sarthi <partha.sarthi@gmail.com>
2018-01-30 16:19:14 +0530

    Added Notificatiomessage model

---
+++ RideShareApp/app/src/main/java/com/digitusrevolution/rideshare/model/common/NotificationMessage.java
@@ -0,0 +1,43 @@
+package com.digitusrevolution.rideshare.model.common;
+
+public class NotificationMessage {
+
+	private String to;
+	private Notification notification = new Notification();
+	
+	public String getTo() {
+		return to;
+	}
+
+	public void setTo(String to) {
+		this.to = to;
+	}
+
+	public Notification getNotification() {
+		return notification;
+	}
+
+	public void setNotification(Notification notification) {
+		this.notification = notification;
+	}
+
+	public class Notification {
+		private String title;
+		private String body;
+		
+		public String getTitle() {
+			return title;
+		}
+		public void setTitle(String title) {
+			this.title = title;
+		}
+		public String getBody() {
+			return body;
+		}
+		public void setBody(String body) {
+			this.body = body;
+		}
+		
+		
+	}
+}
