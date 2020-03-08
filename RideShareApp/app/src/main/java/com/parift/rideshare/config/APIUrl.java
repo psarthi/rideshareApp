94385e4e999d27b234922538ab07fe7cd3411b37
Partha Sarthi <partha.sarthi@gmail.com>
2020-03-08 13:52:14 +0530

    Updated Version Number

--- RideShareApp/app/src/main/java/com/parift/rideshare/config/APIUrl.java
+++ RideShareApp/app/src/main/java/com/parift/rideshare/config/APIUrl.java
@@ -7,11 +7,11 @@ package com.parift.rideshare.config;
 public class APIUrl {
 
     //Environment Specific Parameters
-    public static final String ENV = "DEVELOPMENT";
-    //public static final String ENV = "PRODUCTION";
-    public static final String HOST_NAME = "http://192.168.1.5:8080";
+    //public static final String ENV = "DEVELOPMENT";
+    public static final String ENV = "PRODUCTION";
+    //public static final String HOST_NAME = "http://192.168.1.5:8080";
     //public static final String HOST_NAME = "http://rideshare-test.ap-south-1.elasticbeanstalk.com";
-    //public static final String HOST_NAME = "https://api.parift.in";
+    public static final String HOST_NAME = "https://api.parift.in";
     public static final String BASE_URL_USER_SYSTEM = HOST_NAME + "/RSUserSystem/api";
     public static final String BASE_URL_RIDE_SYSTEM = HOST_NAME + "/RSRideSystem/api/users/{userId}";
     public static final String BASE_URL_BILLING_SYSTEM = HOST_NAME + "/RSBillingSystem/api/users/{userId}";
